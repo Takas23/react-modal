a22a862ace2a88bbc483731a67013eae145d2372
Damian Takashima <d.takas23@gmail.com>
2022-12-25 15:30:00 -0300

    form cheto

--- src/componentsTest/form/useForm.js
+++ src/componentsTest/form/useForm.js
@@ -2,21 +2,66 @@ import { useState } from "react"
 
 export const useForm = (initialForm = {}) => {
 
+
+    //lista de objetos
+    const [listaObj, setListaObj] = useState([{ nombre: "", apellido: "", edad: 0 }])
+
+    //formulario completo
     const [formState, setFormState] = useState(initialForm)
 
 
+    const onInputChange = ({ target: { id, name, value } }) => {
+        let index = id.substring(0, id.indexOf(name))
 
-    const onInputChange = ({ target }) => {
-        const { name, value } = target;
-        setFormState({
-            ...formState,
-            [name]: value
-        })
+        setListaObj([...listaObj].map((obj) => {
+            if (listaObj.indexOf(obj) === index) {
+
+               // Object.assign(obj, { [name]: value })
+
+            }
+        }))
+
+
+
+        //corta el id para conseguir el indice del objeto cargado
+        //console.log(id.substring(0, id.indexOf(name)))
+    }
+
+
+    //agrega un objeto vacio a la lista
+    const addObj = () => {
+        setListaObj([
+            ...listaObj,
+            {}
+        ])
+    }
+
+    // const fillForm = () => {
+    //     setFormState({
+    //         dato1: datos.dato1,
+    //         dato2: datos.dato2,
+    //         dato3: datos.dato3,
+    //         lista: [datos.dato1, datos.dato2, datos.dato3]
+    //     })
+
+    //     console.log("form", formState)
+    // }
+
+    const onSubmitForm = (event) => {
+        event.preventDefault()
+
+        //fillForm()
+
+
+        console.log("lista", listaObj)
     }
 
     return {
         ...formState,
         formState,
-        onInputChange
+        listaObj,
+        onInputChange,
+        addObj,
+        onSubmitForm
     }
 }
